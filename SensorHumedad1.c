980a97ea255ba5db37fba2807eac19af67e25d0a
Alan Lopez-Segovia <ala.lopez.segovia@continental-corporation.com>
2024-12-31 16:25:51 -0600

    Add Humidity sensor1

---
+++ SensorHumedad1.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "SensorHumedad1.h"
+
+int hSensorInit(void)
+{
+    printf("Initializing Humidity sensor 1...")
+    /* Implemantacion para inicializar el sensor 1 de humedad*/
+}
+
+int hGetHumidity(void)
+{
+    printf("Get Humidity from sensor1");
+    /* Implemantacion para obtener humedad del sensor 1*/
+}
