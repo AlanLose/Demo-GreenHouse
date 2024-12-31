ba4a4a792d790fa3d594466b2d2d5573169bb21d
Alan Lopez-Segovia <ala.lopez.segovia@continental-corporation.com>
2024-12-31 16:51:04 -0600

    Add Tempeture sensor 1

---
+++ TempSensor1.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "TempSensor1.h"
+
+int tSensorInit(void)
+{
+    printf("Initializing Temperature sensor 1...")
+    /* Implemantacion para inicializar el sensor 1 de temperatura*/
+}
+
+int tGetTemperature(void)
+{
+    printf("Get Temperature from sensor1");
+    /* Implemantacion para obtener temperatura del sensor 1*/
+}
