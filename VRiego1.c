f03d8ea4e2971da8852e5a5da1f7d79ab9f87721
Alan Lopez-Segovia <ala.lopez.segovia@continental-corporation.com>
2024-12-31 16:33:28 -0600

    Add valve 1

---
+++ VRiego1.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "VRiego1.h"
+
+int valveInit(void)
+{
+    printf("Initializing valve1 configuration...");
+    /* Implemantacion para inicializar la valvula 1*/
+
+}
+
+int valveSet(int value)
+{
+    printf("Setting valve to %d", value);
+    /* Implemantacion para setear el valor de la valvula 1*/
+
+}
