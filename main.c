eec42e080b58f534f2eee0c1ce15780b72833de5
Alan Lopez-Segovia <ala.lopez.segovia@continental-corporation.com>
2024-12-31 16:41:11 -0600

    Add basic fucntionality (Humidity + valve)

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include "stdio.h"
+#include "SensorHumedad1.h"
+#include "VRiego1.h"
+
+#define SETPOINT 60
 
 int main (void)
 {
-    printf("main function");
+    int Humidity = 0;
+    printf("Initializing system...");
+    (void)hSensorInit();
+    (void)valveInit();
+
+    Humidity = hGetHumidity();
+
+    if(Humidity < SETPOINT)
+    {
+        valveSet(1);
+    }else {
+        //nothing to do
+    }
+
     return 0;
 }
