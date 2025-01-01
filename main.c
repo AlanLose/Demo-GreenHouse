f9fea4042cc60dbfeae105225c314f0e795af48a
Alan Lopez-Segovia <ala.lopez.segovia@continental-corporation.com>
2024-12-31 19:08:35 -0600

    Add Temp and AC functionality

--- main.c
+++ main.c
@@ -2,18 +2,23 @@
 #include "SensorHumedad1.h"
 #include "VRiego1.h"
 
-#define SETPOINT 60
+#define HSETPOINT 60 //Porcentaje
+#define TSETPOINT 30 //grados_Celsius
 
 int main (void)
 {
+    int Temperature = 0;
     int Humidity = 0;
     printf("Initializing system...");
     (void)hSensorInit();
+    (void)tSensorInit();
     (void)valveInit();
+    (void)acInit();
 
+    Temperature = tGetTemperature();
     Humidity = hGetHumidity();
 
-    if(Humidity < SETPOINT)
+    if ( (Humidity < HSETPOINT) || (Temperature > TSETPOINT) )
     {
         valveSet(1);
     }else {
